4e55992b1ffe944a6bec03349b14cbfffd750daf
João Pedro <pedrosant1905@gmail.com>
2024-07-23 19:10:54 -0300

    modificação no código do arduino

---
+++ main_arduino/main.ino
@@ -0,0 +1,17 @@
+#include "temperatura.h"
+#include "turbidez.h"
+#include "vibracao.h"
+
+void setup() {
+  //setupTemperatura();
+  setupVibracao();
+  //setupTurbidez();
+  //setupOxigenacao();
+}
+
+void loop() {
+  //loopTemperatura();
+  loopVibracao();
+  //loopTurbidez();
+  //loopOxigenacao();
+}
---
+++ main_arduino/vibracao.h
@@ -0,0 +1,9 @@
+#ifndef VIBRACAO_H
+#define VIBRACAO_H
+
+extern const int pinoSensor;
+
+void setupVibracao();
+void loopVibracao();
+
+#endif
